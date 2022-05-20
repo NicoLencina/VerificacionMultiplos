510935e8ee8d674aac38b7c5a4ee4b9d7a03bc36
Lencina Nicolas Angelo <104397441+NicoLencina@users.noreply.github.com>
2022-05-20 00:08:02 -0300

    corregido

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
 //traigo datos del HTML
 let primerNum: number = document.getElementById("primerDato");
 let segundoNum: number = document.getElementById("segundoDato");
+let resultado: boolean;
 let buttonOk = document.getElementById("buttonOk");
 
 //intento resolver con funcion
-function isMultiple(x: number, y: number): boolean {
-  let resultado: boolean = false;
-  resultado = x % y == 0;
-  return resultado;
+function esMultiplo(x: number, y: number): boolean {
+  let resul: number;
+  resul = y % x;
+  if (resul === 0) {
+    return (resultado = true);
+  } else {
+    return (resultado = false);
+  }
 }
 
 //Al presionar el usuario el boton :
 buttonOk.addEventListener("click", () => {
   let num1: number = Number(primerNum.value);
   let num2: number = Number(segundoNum.value);
-  let resultado: boolean = Boolean(primerNum % segundoNum == 0);
+  esMultiplo(num1, num2);
   if (resultado === true) {
     console.log("El numero ", num1, "si es multiplo de ", num2);
   } else {
